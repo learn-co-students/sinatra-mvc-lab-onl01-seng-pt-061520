519d3a41913b1e2b66706ac49685d27818a11194
ajkelso <ajkelso@gmail.com>
2020-09-11 15:09:41 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        @analyzed_text = PigLatinizer.new
+
+        erb :converted_text
+    end
+
+
 end
